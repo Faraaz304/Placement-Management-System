a685e652833be69511e3f775afd0cb4bef61ecfb
Raahim <144481724+Faraaz304@users.noreply.github.com>
2025-01-11 13:05:30 +0530

    chagned the link

--- frontend/src/app/Login/page.js
+++ frontend/src/app/Login/page.js
@@ -24,7 +24,7 @@ export default function Login() {
     setError("");
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/login", {
+      const response = await fetch("https://placement-management-system-backend.vercel.app/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- frontend/src/app/Signup/page.js
+++ frontend/src/app/Signup/page.js
@@ -31,7 +31,7 @@ export default function Signup() {
     }
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/signup', {
+      const response = await fetch('https://placement-management-system-backend.vercel.app/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
